1f81d1d27538d90493622255262b791f77f7b4be
takaomiyamashita <ty.8479@gmail.com>
2021-04-25 23:29:38 +0900

    ショップ一覧表示するために、まずuser_idを表示した

---
+++ app/Http/Controllers/ShopListController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Item;
+use App\ShopList;
+use Auth;
+use Illuminate\Support\Facades\DB;
+
+class ShopListController extends Controller
+{
+    function index(){
+        $shop_owner_id = DB::table('item')->distinct()->select('user_id')->get();
+
+        return view("shop_list", [
+            "shop_owner_id" => $shop_owner_id
+        ]);
+    }
+}
