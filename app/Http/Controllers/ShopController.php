1a7de00c383e3e863b40b931e314a1aad906d9b9
takaomiyamashita <ty.8479@gmail.com>
2021-05-22 10:44:35 +0900

    再度stripeを組み込む。決済処理のみ成功

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -31,37 +31,27 @@ class ShopController extends Controller
     }
 
     public function edit(Request $request) {  
-        // $post = Item::find(1);
-        // if(is_null($_POST)) {
-        //     return redirect('/list');
-        // }
-
-        // user_idのうちの一番古いidがupdateされていることになる
-        // $item = Item::find($request->id);
-
-        // $item->user_id = $request->buyer_id;
-        // $item->save();
         $item = new Item;
         $item->where('id', $request->id)->update(['user_id' => $request->buyer_id]);
 
-        // try {
-        //     Stripe::setApiKey(env('STRIPE_SECRET'));
+        try {
+            Stripe::setApiKey(env('STRIPE_SECRET'));
 
-        //     $customer = Customer::create(array(
-        //         'email' => $request->stripeEmail,
-        //         'source' => $request->stripeToken
-        //     ));
+            $customer = Customer::create(array(
+                'email' => $request->stripeEmail,
+                'source' => $request->stripeToken
+            ));
 
-        //     $charge = Charge::create(array(
-        //         'customer' => $customer->id,
-        //         'amount' => 1000,
-        //         'currency' => 'jpy'
-        //     ));
+            $charge = Charge::create(array(
+                'customer' => $customer->id,
+                'amount' => 1000,
+                'currency' => 'jpy'
+            ));
 
-		// 	// return redirect("/list");
-        // } catch (\Exception $ex) {
-        //     return $ex->getMessage();
-        // }
+			// return redirect("/list");
+        } catch (\Exception $ex) {
+            return $ex->getMessage();
+        }
 
         return redirect('/list');
     }
