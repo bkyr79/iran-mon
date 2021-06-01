5de57caf56dc116985de75227f9fff6c4af50f05
takaomiyamashita <ty.8479@gmail.com>
2021-06-01 22:59:11 +0900

    charge.bladeをblade活用に書き換えた

--- resources/views/charge.blade.php
+++ resources/views/charge.blade.php
@@ -1,39 +1,31 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>stripe決済</title>
-</head>
-<body>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+@extends('layouts.typical')
+@section('title', 'stripe決済')
 
-<form action="{{ asset('charge') }}" method="POST" name="charge_form">
-  {{ csrf_field() }}
-    <script
-      src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-      data-key="{{ env('STRIPE_KEY') }}"
-      data-amount="{{ $goods_price }}"
-      data-name="Stripe Demo"
-      data-label="はい"
-      data-description="Online course about integrating Stripe"
-      data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-      data-locale="auto"
-      data-currency="JPY">
-    </script>
-    
-<button type="submit" class="stripe-button-el-dummy" id="stripe-button-el" style="visibility: visible;"><span style="display: block; min-height: 30px;">はい</span></button>
-<script>
-  document.getElementsByClassName("stripe-button-el")[0].style.display = 'none';
-  document.getElementsByClassName("stripe-button-el-dummy")[0].style.display = 'none';
+@section('body')
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
-  $(function() {
-    document.getElementById("stripe-button-el").click();
-  });
-</script>
-</form>
+  <form action="{{ asset('charge') }}" method="POST" name="charge_form">
+    {{ csrf_field() }}
+      <script
+        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+        data-key="{{ env('STRIPE_KEY') }}"
+        data-amount="{{ $goods_price }}"
+        data-name="Stripe Demo"
+        data-label="はい"
+        data-description="Online course about integrating Stripe"
+        data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+        data-locale="auto"
+        data-currency="JPY">
+      </script>
+      
+  <button type="submit" class="stripe-button-el-dummy" id="stripe-button-el" style="visibility: visible;"><span style="display: block; min-height: 30px;">はい</span></button>
+  <script>
+    document.getElementsByClassName("stripe-button-el")[0].style.display = 'none';
+    document.getElementsByClassName("stripe-button-el-dummy")[0].style.display = 'none';
 
-
-</body>
-</html>
+    $(function() {
+      document.getElementById("stripe-button-el").click();
+    });
+  </script>
+  </form>
+@endsection
