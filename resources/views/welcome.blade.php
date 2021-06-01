7d79c07446e713c47622ac931ac60f5e37ba8225
takaomiyamashita <ty.8479@gmail.com>
2021-06-01 16:24:25 +0900

    welcome.cssを整理した

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,87 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ira-mon</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
+        <link href="{{ asset('/css/welcome.css') }}" rel="stylesheet" type="text/css">
+
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: rgb(40, 167, 69, 0.04);
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            .easy-login {
-                position: relative;
-                bottom: 4px;
-                right: 100px;
-                float: left;
-                padding: 4px 6px;
-                background: white;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: bold;
-                letter-spacing: .1rem;
-                border-color: rgb(98, 108, 112, 0.5);
-                border-radius: 3px;
-                cursor: pointer;
-            }
-            .login {
-                position: relative;
-                right: 90px;               
-            }
-            .register {
-                position: relative;
-                bottom: 22px;                
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
