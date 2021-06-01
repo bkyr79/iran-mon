7f39413f93f41991076b71eb2ac870a38781fc0e
takaomiyamashita <ty.8479@gmail.com>
2021-06-01 16:18:41 +0900

    upload_form.cssを整理した

--- resources/views/upload_form.blade.php
+++ resources/views/upload_form.blade.php
@@ -4,109 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-    ul.header-dropmenu {
-    list-style: none;
-    width: 100%;
-    height: 40px;
-    margin: 0 auto;
-    padding: 0;
-    display: block;
-    table-layout: fixed;
-    }
-    ul.header-dropmenu > li {
-    position: relative;
-    /* display: table-cell; */
-    vertical-align: middle;
-    border: 1px solid #f8f8f8;
-    background: #f8f8f8; /* 背景色*/
-    }
-    ul.header-dropmenu li a {
-    display: block;
-    text-align: center;
-    line-height: 40px;
-    font-weight: bold;
-    text-decoration: none;
-    font-size: 14px;
-    }
-    ul.header-dropmenu li ul {
-    visibility: hidden;
-    width: 100%;
-    list-style: none;
-    position: absolute;
-    top: 100%;
-    left: -1px;
-    margin: 0;
-    padding: 0;
-    border: 1px solid #222; /* マウスオーバー時の枠線 */
-    border-top: none;
-    border-right: none;
-    }
-    ul.header-dropmenu li:hover ul {
-    visibility: visible;
-    }
-    ul.header-dropmenu li ul li {
-    background: #fff;
-    transition: all .2s ease;
-    }
-    ul.header-dropmenu > li:hover {
-    background: #fff;
-    border: 1px solid #222; /* マウスオーバー時の枠線 */
-    border-bottom: none;
-    border-right: none;
-    }
-    ul.header-dropmenu li:hover ul li:hover {
-    background: #f8f8f8;
-    }
-    .menu-btn:hover {
-    cursor: default;
-    }
-    .file-select {
-      margin: 9px 0px;
-    }
-    .name-input {
-      position: relative;
-      right: 8px;
-      margin-bottom: 5px;    
-    }  
-    .price-input {
-      margin-top: 0px;
-    }
-    .register-submit {
-      position: relative;
-      left: 100px;
-      margin-top: 8px;
-    }
-    .file-register {
-    display       : inline-block;
-    border-radius : 3%;
-    font-size     : 14pt;
-    text-align    : center;
-    cursor        : pointer;
-    padding       : 8px 12px;
-    background    : rgba(0, 127, 255, 0.1);
-    color         : rgba(0, 127, 255, 0.87);
-    line-height   : 1em;
-    transition    : .3s;
-    border        : 2px solid rgba(0, 127, 255, 0.87);
-    float         : right;
-    position      : relative;
-    top           : 3px;
-    left          : 90px;
-    }
-    .label-and-regbtn {
-      display    : inline-block;
-      text-align : center;
-      position   : relative;
-      bottom     : 35px;
-    }
-    .pre-image {
-      position : relative;
-      bottom   : 30px;
-    }
-    </style>
-
+    <title>商品登録ページ</title>
+    <link href="{{ asset('/css/upload_form.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
 
@@ -127,9 +26,9 @@
 </div>
 @endif
 
-<div style="text-align:right; padding-top:5px; padding-right:9px; color:gray;">{{ Auth::user()->name }}さんがログイン中</div>
-<div style="text-align:center; margin-top:50px; margin-bottom:0px; display:inleine-block; font-size:19px;">{{ Auth::user()->name }}さんのuploadページ</div>
-<div style="width:120px; position:absolute; right:0px; ">
+<div class="login-state">{{ Auth::user()->name }}さんがログイン中</div>
+<div class="uploadpage-title">{{ Auth::user()->name }}さんの商品登録ページ</div>
+<div class="acc-menu">
 <ul class="header-dropmenu">
   <li>
     <a href="#" class="menu-btn">メニュー</a>
@@ -147,9 +46,9 @@
     @csrf
 </form>
 
-<hr style="display:block; margin-top:41px;"/>
+<hr class="sepa-border"/>
 
-<div style="text-align:center; margin-top:20px;">
+<div class="uploadpage-content">
 <form
   method="post"
   action="{{ route('upload_image') }}"
@@ -159,10 +58,10 @@
   <div class="file-select"><input type="file" name="file_path" accept="image/png, image/jpeg" onchange="previewImage(this);"></div>
   <div class="name-input"><label for="">商品名：</label><input type="text" name="goods_name" value="{{ old('goods_name') }}"></div>
   <div class="price-input"><label for="">価格：</label><input type="text" name="goods_price" value="{{ old('goods_price') }}"></div>
-  <div class="label-and-regbtn"><p style="float:left; position:relative; right:100px; top:64px;">Preview:</p><p><input type="submit" value="登録" class="file-register"></p></div>
+  <div class="label-and-regbtn"><p class="preview-title">Preview:</p><p><input type="submit" value="登録" class="file-register"></p></div>
 </form>
 <div class="pre-image">
-<img id="preview" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" style="width: 246px; height: 246px;">
+<img id="preview" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="preview-img">
 </div>
 </div>
 
