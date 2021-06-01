26709e486bc4fd34aa787b9cfe1ed0eb704370f1
takaomiyamashita <ty.8479@gmail.com>
2021-06-01 23:24:27 +0900

    shop_list.bladeをblade活用に書き換えた

--- resources/views/shop_list.blade.php
+++ resources/views/shop_list.blade.php
@@ -1,53 +1,48 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ショップ一覧ページ</title>
-    <link href="{{ asset('/css/shop_list.css') }}" rel="stylesheet" type="text/css">
-</head>
-<body>
+@extends('layouts.typical')
+@section('title', 'ショップ一覧ページ')
 
-<!-- Bootstrap導入 -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!-- Bootstrap Javascript(jQuery含む) -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+@section('stylesheet')
+  <link href="{{ asset('/css/shop_list.css') }}" rel="stylesheet" type="text/css">
+@endsection
 
-<div class="login-state">{{ Auth::user()->name }}さんがログイン中</div>
-<div class="shoplist-title">ショップ一覧</div>
-<div class="acc-menu">
-<ul class="header-dropmenu">
-  <li>
-    <a href="#" class="menu-btn">メニュー</a>
-    <ul>
-      <li><a href="{{ url('/list') }}">マイページに戻る</a></li>
-      <li><a href="{{ route('logout') }}"
-        onclick="event.preventDefault();
-        document.getElementById('logout-form').submit();">ログアウト</a></li>
-    </ul>
-  </li>
-</ul>
-</div>
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
+@section('body')
+  <!-- Bootstrap導入 -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- Bootstrap Javascript(jQuery含む) -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-<hr class="sepa-border"/>
-<div class="shoplist-content">
-@foreach($shop_owner_id as $one_id)
-    <form action="/shop" method="post">
-    @csrf
-    @if($one_id->id <> Auth::user()->id )
-    <input type="submit" class="shop-name" value="{{ $one_id->name }}さんのショップ">
-    <input type="hidden" name="owner_id" value="{{ $one_id->id }}">
-    <input type="hidden" name="owner_name" value="{{ $one_id->name }}">
-    @endif
-    </form>
-@endforeach
-</div>
+  <div class="login-state">{{ Auth::user()->name }}さんがログイン中</div>
+  <div class="shoplist-title">ショップ一覧</div>
+  <div class="acc-menu">
+  <ul class="header-dropmenu">
+    <li>
+      <a href="#" class="menu-btn">メニュー</a>
+      <ul>
+        <li><a href="{{ url('/list') }}">マイページに戻る</a></li>
+        <li><a href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+          document.getElementById('logout-form').submit();">ログアウト</a></li>
+      </ul>
+    </li>
+  </ul>
+  </div>
+  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+      @csrf
+  </form>
 
-</body>
-</html>
+  <hr class="sepa-border"/>
+  <div class="shoplist-content">
+  @foreach($shop_owner_id as $one_id)
+      <form action="/shop" method="post">
+      @csrf
+      @if($one_id->id <> Auth::user()->id )
+      <input type="submit" class="shop-name" value="{{ $one_id->name }}さんのショップ">
+      <input type="hidden" name="owner_id" value="{{ $one_id->id }}">
+      <input type="hidden" name="owner_name" value="{{ $one_id->name }}">
+      @endif
+      </form>
+  @endforeach
+  </div>
+@endsection
