cfed3158672c3dab8133ab292902d87d70afe1ef
takaomiyamashita <ty.8479@gmail.com>
2021-06-02 22:53:40 +0900

    cssの整理

---
+++ resources/views/layouts/filelist.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @yield('stylesheet')
+    <link href="{{ asset('/css/dropmenu.css') }}" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<!-- Bootstrap導入 -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<!-- Bootstrap Javascript(jQuery含む) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<!-- ユーザー名を表示 -->
+<div class="login-state">{{ Auth::user()->name }}さんがログイン中</div>
+@yield('header')
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
+<hr class="sepa-border"/>
+@yield('div')
+<div class="top-pagi">{{ $images->links() }}</div>
+@yield('content')
+<div class="bottom-pagi">{{ $images->links() }}</div>
+
+@yield('footer')
+</body>
+</html>
