7dda1dd07ff0b906e0925d914fce4e55be93e164
takaomiyamashita <ty.8479@gmail.com>
2021-06-02 22:22:09 +0900

    ルーティングの整理

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,29 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/guest_login', 'Auth\RegisterController@guestUserCreate')->name('guest_login');
-
-Route::get('/form', 
-	[App\Http\Controllers\ItemController::class, "show"]
-	)->name("upload_form");
-
-Route::post('/upload', 
-	[App\Http\Controllers\ItemController::class, "upload"]
-	)->name("upload_image");
-
-Route::get('/list',
-    [App\Http\Controllers\ItemListController::class, "show"]
-    )->name("item_list");
-Auth::routes();
+Route::view('/', 'welcome');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/shop', function () {
-    return view('shop');
-    });
+Route::get('/guest_login', 'Auth\RegisterController@guestUserCreate')->name('guest_login');
 
-Route::post('/shop', 
-    [App\Http\Controllers\ShopController::class, "show"]
-    )->name("item_list");
+Route::prefix('/upload')->group(function () {
+    Route::get('', 'ItemController@show')->name("upload_form");
+    Route::post('', 'ItemController@upload')->name("upload_image");    
+});
 
-Route::get('/shop', 
-    [App\Http\Controllers\ShopController::class, "index"]
-    );
+Route::prefix('/list')->group(function () {
+    Route::get('', 'ItemListController@show')->name("item_list");
+    Route::post('', 'ShopController@receiveInfoGoodsToBuy');    
+});
 
-Route::post('/list', 'ShopController@receiveInfoGoodsToBuy');
-
-Route::get('/shoplist', 'ShopListController@index');
+Route::prefix('/shop')->group(function () {
+    Route::post('', 'ShopController@show')->name("item_list");
+    Route::get('', 'ShopController@index');    
+});
 
 Route::post('/itemlist', 'ItemListController@edit');
+
+Route::get('/shoplist', 'ShopListController@index');
 
 Route::get('/delete_list', 'ItemListController@deleteList')->name('delete_list');
 
@@ -45,3 +31,4 @@ Route::post('/delete', 'ItemListController@delete');
 
 Route::post('/charge', 'ChargeController@chargeAndChangeOwnership');
 
+Auth::routes();
