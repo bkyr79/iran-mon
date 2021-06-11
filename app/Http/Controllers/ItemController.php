f3cb86ccc50de12f76fe7dbe52bdb28ac59ece0d
takaomiyamashita <ty.8479@gmail.com>
2021-06-11 23:18:04 +0900

    定義のダブり箇所削除

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -51,7 +51,6 @@ class ItemController extends Controller
       $form = $request->all();
 
       //s3アップロード開始
-      $upload_image = $request->file('file_path');
       // バケットの`myprefix`フォルダへアップロード
       $path = Storage::disk('s3')->putFile('myprefix', $upload_image);
       // アップロードした画像のフルパスを取得
