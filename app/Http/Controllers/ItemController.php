898dabfca2990dec153f49f82778ed79bbcee192
takaomiyamashita <ty.8479@gmail.com>
2021-06-15 13:57:43 +0900

    S3画像削除(複数)

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -55,7 +55,8 @@ class ItemController extends Controller
       // バケットの`myprefix`フォルダへアップロード
       $path = Storage::disk('s3')->putFile('myprefix', $upload_image, 'public');
       // アップロードした画像のフルパスを取得
-      $item->file_path = Storage::disk('s3')->url($path);
+      // $item->file_path = Storage::disk('s3')->url($path);
+			$item->file_path = $path;
 
 			$user_id = Auth::id(); //『Auth::id()』でログイン中のidを取得できる
 			$goods_name = $request->input('goods_name');
