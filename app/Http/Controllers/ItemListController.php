54eb3aaa7cf43ebf13a6006f095502442dfc3e1d
takaomiyamashita <ty.8479@gmail.com>
2021-06-16 13:50:38 +0900

    S3画像削除(複数)

--- app/Http/Controllers/ItemListController.php
+++ app/Http/Controllers/ItemListController.php
@@ -68,10 +68,10 @@ class ItemListController extends Controller
         $disk = Storage::disk('s3');
         for ($i=0; $i<count($checked_ids, COUNT_RECURSIVE)-1; $i++) {
             $items = Item::find($checked_ids[$i]);
-            // \Log::debug(print_r($items));
+            \Log::debug(print_r($items));
             // \Log::debug(print_r(count($checked_ids, COUNT_RECURSIVE)-1));
             // dump(count($checked_ids, COUNT_RECURSIVE)-1);
-
+            dump($items);
             $disk->delete($items->file_path);
         }
 
