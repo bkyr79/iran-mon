68f5e36dcb7fae6c3657dd28790877ceef93599f
takaomiyamashita <ty.8479@gmail.com>
2021-06-16 15:14:12 +0900

    S3画像削除(複数)

--- app/Http/Controllers/ItemListController.php
+++ app/Http/Controllers/ItemListController.php
@@ -66,16 +66,16 @@ class ItemListController extends Controller
 
         // そのidのデータを全て取得し、そのidの写真のfile_pathをS3から削除する
         $disk = Storage::disk('s3');
-        // for ($i=0; $i<count($checked_ids, COUNT_RECURSIVE)-1; $i++) {
-            $items = Item::find($checked_ids[0]);
+        for ($i=0; $i<count($checked_ids, COUNT_RECURSIVE)-1; $i++) {
+            $items = Item::find($checked_ids[$i]);
 
             \Log::debug(print_r($items));
             // \Log::debug(print_r(Item::find($checked_ids[1])));
 
             dump($items);
             // dump(Item::find($checked_ids[1]));
-            // $disk->delete($items->file_path);
-        // }
+            $disk->delete($items->file_path);
+        }
 
 
 
