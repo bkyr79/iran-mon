e90fb202ebfef2c5497cf555178495915a1c0920
takaomiyamashita <ty.8479@gmail.com>
2021-06-19 22:49:24 +0900

    ショップリスト表示を、アイテム所有のショップのみにした

--- app/Http/Controllers/ShopListController.php
+++ app/Http/Controllers/ShopListController.php
@@ -27,12 +27,12 @@ class ShopListController extends Controller
         // $hukumukadouka = in_array($userid_of_items[$i], $id_of_users, true);
 
 
-        $whether_has_items = $users->whereIn('id', $userid_of_items)->get();
+        // $whether_has_items = $users->whereIn('id', $userid_of_items)->get();
 
-// dump($whether_has_items);
+dump($$userid_of_items);
 // echo $userid_of_items;
 // dump($id_of_users);
-// die;
+die;
 
         return view("shop_list", [
             "shop_owner_id" => $shop_owner_id,
