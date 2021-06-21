5d98cdbf93147ff8506e7c17b4da059941c420f3
takaomiyamashita <ty.8479@gmail.com>
2021-06-21 09:16:55 +0900

    簡単ログイン用ダイアログ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,34 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>ira-mon</title>
-        <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
-        <script src="http://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <link href="{{ asset('/css/welcome.css') }}" rel="stylesheet" type="text/css">
 
-        <style>
-        /* ダイアログ */
-        .ui-icon ui-icon-alert {
-            float: left;
-            margin: 0 7px 20px 0;
-        }
-        .ui-dialog-titlebar {
-            color: #ffffff;
-            background: green;
-        }
-        </style>
     </head>
     <body>
-        <!-- <div id="dialog-confirm" title="">
-        <p>
-        <span class="ui-icon ui-icon-alert"></span>
-        再ログインできませんが、よろしいですか？
-        </p>
-        </div> -->
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -40,7 +20,7 @@
                         <a href="{{ url('/list') }}">My page</a>
                     @else
                         <form action="{{ url('guest_login') }}">
-                        <button type="submit" class="easy-login" id="easyLogin">簡単ログイン</button>
+                        <input type="submit" value="簡単LOGIN" class="easy-login">
                         </form>
 
                         <a href="{{ route('login') }}" class="login">Login</a>
@@ -62,24 +42,5 @@
                 </div>
             </div>
         </div>
-        <!-- ダイアログ表示の実装 -->
-        <script>
-            // $(function easyLoginDialog() {
-            // $( "#dialog-confirm" ).dialog({
-            //     modal: true,
-            //     buttons: {
-            //     "はい": function() {
-            //         $( this ).dialog( "close" );
-            //     },
-            //     "いいえ": function() {
-            //         $( this ).dialog( "close" );
-            //     }
-            //     }
-            // });
-            // });
-            
-            // let button = document.getElementById('easyLogin');
-            // button.addEventListener('click', easyLoginDialog);
-        </script>
     </body>
 </html>
