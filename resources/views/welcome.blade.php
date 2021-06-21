705908421dce42f790dcfef1e4d1696c00a30cd2
takaomiyamashita <ty.8479@gmail.com>
2021-06-21 11:18:54 +0900

    ログイン画面修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,10 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
-        <link href="{{ asset('/css/welcome.css') }}" rel="stylesheet" type="text/css">
-
         <!-- Bootstrap導入 -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <link href="{{ asset('/css/welcome.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body>
         <!-- Bootstrap Javascript(jQuery含む) -->
