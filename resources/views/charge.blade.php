b715c9f9050e0cf2f8fb2e87e68831358709e592
takaomiyamashita <ty.8479@gmail.com>
2021-07-03 14:32:58 +0900

    商品購入直後の待ち画面にローダーを表示させた

--- resources/views/charge.blade.php
+++ resources/views/charge.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.typical')
 @section('title', 'stripe決済')
 
+@section('stylesheet')
+  <link href="{{ asset('/css/loader_after_buy.css') }}" rel="stylesheet" type="text/css">
+@endsection
+
 @section('body')
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
@@ -18,7 +22,20 @@
         data-currency="JPY">
       </script>
       
+      <!-- 購入直後にローダーを表示させる -->
+      <script>
+        $(function(){
+          setTimeout(function(){
+            $("#loading").fadeIn();
+          },10000);
+        });
+      </script>
+
   <button type="submit" class="stripe-button-el-dummy" id="stripe-button-el" style="visibility: visible;"><span style="display: block; min-height: 30px;">はい</span></button>
+  
+  <!-- ローダー -->
+  <div id="loading" style="display: none"><img src="/storage/uploads/ajax-loader.gif"></div>
+
   <script>
     document.getElementsByClassName("stripe-button-el")[0].style.display = 'none';
     document.getElementsByClassName("stripe-button-el-dummy")[0].style.display = 'none';
